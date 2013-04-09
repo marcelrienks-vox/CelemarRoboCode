851635962862cb648d766ffc218cdd71c69391a9
Celemar <marcelrienks@gmail.com>
2013-04-09 07:13:06 +0200

    * Created targeting library * Created locked and unlocked scan sweeps logic * Created logic to lock onto Target * Created logic to oscillate scans * Implemented Targeting in WallE

---
+++ CelemarRoboCode/Libraries/Targeting.cs
@@ -0,0 +1,76 @@
+﻿using Robocode;
+
+namespace Celemar.Libraries {
+	class Targeting {
+		public AdvancedRobot Robot { get; set; }
+		private int ScanDirection = -1;
+
+		public Targeting(AdvancedRobot robot) {
+			Robot = robot;
+		}
+
+		/// <summary>
+		/// Locked Scan for target.
+		/// This performs a 45 degree sweep to the left, then sweeps right at 45 degree increments.
+		/// NOTE: this is an unlocking scan, other operations can be executed between right sweeps
+		/// </summary>
+		public void UnlockedScanForTarget() {
+			//Sweep 45 degrees to the left
+			Robot.TurnRadarLeft(45);
+
+			//Sweep right by 45 degrees each time
+			while (true) {
+				Robot.TurnRadarRight(45);
+			}
+		}
+
+		/// <summary>
+		/// Locked Scan for target.
+		/// This performs a 45 degree sweep to the left, then sweeps a Full 360 to the right from the original position.
+		/// NOTE: this is a locking scan, no other operations can be executed while the 360 degree sweep is executing
+		/// </summary>
+		public void LockedScanForTarget() {
+			//Sweep 45 degrees to the left
+			Robot.TurnRadarLeft(45);
+
+			//Sweep 360 degrees to the right (405 degrees compensates for the 45 degree left sweep)
+			Robot.TurnRadarRight(405);
+		}
+
+		/// <summary>
+		/// Locks the on to target.
+		/// When a target is scanned, the radar sweeps around in the oposite direction only by 10 degrees.
+		/// This will hopefully maintain a lock on the target.
+		/// This is best used with a single enemy.
+		/// </summary>
+		/// <param name="bearing">The bearing.</param>
+		public void LockOnToTarget(double bearing) {
+			//Lock on to target
+			Robot.TurnRadarRight(Robot.Heading - Robot.RadarHeading + bearing);
+
+			//swap scan direction
+			ScanDirection *= -1;
+
+			//scan
+			Robot.TurnRadarLeft(10 * ScanDirection);
+		}
+
+		/// <summary>
+		/// Oscillates the scan.
+		/// When a target is scanned, the radar sweeps around in the oposite direction a full 360 degrees.
+		/// This will allow other potentially closer targets to be scanned.
+		/// This is best used with multiple enemies.
+		/// </summary>
+		/// <param name="bearing">The bearing.</param>
+		public void OscillateScan(double bearing) {
+			//Lock on to target
+			Robot.TurnRadarRight(Robot.Heading - Robot.RadarHeading + bearing);
+
+			//swap scan direction
+			ScanDirection *= -1;
+
+			//scan
+			Robot.TurnRadarLeft(360 * ScanDirection);
+		}
+	}
+}
--- CelemarRoboCode/Robots/WallE.cs
+++ CelemarRoboCode/Robots/WallE.cs
@@ -1,21 +1,45 @@
 ﻿using System.Drawing;
+using Celemar.Libraries;
 using Robocode;
 
 namespace Celemar {
-	class WallE : Robot {
+	class WallE : AdvancedRobot {
+		private Targeting targeting = null;
+
+		/// <summary>
+		/// The main method in every robot
+		/// </summary>
 		public override void Run() {
+			//initialise Robot
 			InitialiseRobot();
+
+			//Start locked scan for targets
+			targeting.LockedScanForTarget();
 		}
 
 		/// <summary>
 		/// Initialises the robot.
 		/// </summary>
 		private void InitialiseRobot() {
+			//Access the Targeting library
+			targeting = new Targeting(this);
+
+			//Set colours
 			SetColors(Color.Orange, Color.DarkSlateGray, Color.LightGray, Color.Red, Color.LightGreen);
+
+			//Unlock radar from Gun
+			IsAdjustRadarForGunTurn = true;
 		}
 
-		private void ScanForOpponent() {
-
-		}
+		#region Events
+		/// <summary>
+		/// Called when [scanned robot].
+		/// </summary>
+		/// <param name="e">The e.</param>
+		public override void OnScannedRobot(ScannedRobotEvent e) {
+			//Maintain lock on target
+			targeting.LockOnToTarget(e.Bearing);
+		} 
+		#endregion
 	}
 }
